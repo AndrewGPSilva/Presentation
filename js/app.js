2aad70f9622e8741519b39b4ae381af45a05b232
Andrew Gomes <andrewgomes1328@gmail.com>
2023-08-03 20:57:10 -0300

    botão para voltar ao topo adicionado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 window.sr = ScrollReveal({reset: true});
 sr.reveal('.section', { durantion: 500 });
 sr.reveal('.card', {durantion: 500});
+
+function retornarAoTopo() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
