4ec3f0f8e1a051b500a23c4644b537250be34e13
Andrew Gomes <andrewgomes1328@gmail.com>
2023-08-24 20:44:37 -0300

    adicionando coisas novas

--- js/sobre.js
+++ js/sobre.js
@@ -4,3 +4,15 @@ const btn = document.getElementById("quer-me-conhecer");
 btn.addEventListener("click", () => {
     texto.classList.toggle("sobre-infos-ativado");
 })
+
+const music = new Audio('/musicas/Imagine Dragons - Demons (Official Music Video)(MP3_160K).mp3');
+const musicBt = document.querySelector('#music');
+
+musicBt.addEventListener('click', ()=> {
+    if(music.paused){
+        music.play();
+        music.volume = 0.5;
+    } else {
+        music.pause();
+    }
+})
